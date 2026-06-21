5f7cf0b0f39898f1cb884a87840ad27e9938fcfb
Mateus <mateusguima95@gmail.com>
2026-06-21 15:45:29 -0300

    consolida os 3 arquivos em um unico dataframe

--- consolidador.py
+++ consolidador.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import glob
 
-df = pd.read_excel("dados/entrada/vendas_Janeiro_2026.xlsx")
+arquivos = glob.glob("dados/entrada/*.xlsx")
 
-print(df.head())
+lista_dfs = []
+
+for arquivo in arquivos:
+    df = pd.read_excel(arquivo)
+    lista_dfs.append(df)
+
+df_consolidado = pd.concat(lista_dfs)
+
+print(df_consolidado.shape)
